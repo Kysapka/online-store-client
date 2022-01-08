ac993455f87108409a66a5c5a43a56db956054ce
Артем <kysapka@gmail.com>
2022-01-08 16:22:10 +0300

    modals window fixed

--- src/components/modals/createDevice.js
+++ src/components/modals/createDevice.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Form} from "react-bootstrap";
+import {Context} from "../../index";
+import {Dropdown} from "bootstrap";
 
 const CreateDevice = ({show, onHide}) => {
 
+    const {device} = useContext(Context)
+
     const ModalWindow = () => {
-        return (<div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLabel">Добавить устройство</h5>
-                    <button type="button" className="btn-close" onClick={onHide}
-                            aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                    <Form>
-                        <Form.Control
-                            placeholder="Введите название устройства"
-                        />
-                    </Form>
-                </div>
-                <div className="modal-footer">
-                    <button className="btn btn-outline-danger" onClick={onHide}>Закрыть</button>
-                    <button className="btn btn-outline-success" onClick={onHide}>Добавить</button>
+        return (
+
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Добавить устройство</h5>
+                        <button type="button" className="btn-close" onClick={onHide}
+                                aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <Form>
+                            <Dropdown>
+                                ccc
+                            </Dropdown>
+                                {/*<Form.Control*/}
+                                {/*    placeholder="Введите название устройства"*/}
+                                {/*/>*/}
+                        </Form>
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn btn-outline-danger" onClick={onHide}>Закрыть</button>
+                        <button className="btn btn-outline-success" onClick={onHide}>Добавить</button>
+                    </div>
                 </div>
             </div>
-        </div>)
+    )
 
     }
 
-    return show && <ModalWindow/>
-};
+    return show &&
+        <ModalWindow/>
+    };
 
-export default CreateDevice;
+    export default CreateDevice;
