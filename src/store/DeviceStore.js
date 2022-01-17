e938febd4941a3260cce4f3ad65fb09765818d7c
Артем <kysapka@gmail.com>
2022-01-17 14:23:58 +0300

    DAL API REQUESTS added

--- src/store/DeviceStore.js
+++ src/store/DeviceStore.js
@@ -3,69 +3,67 @@ import {makeAutoObservable} from "mobx"
 export default class DeviceStore {
     constructor() {
         this._types = [
-            {id: 1, name: "Холодильники"},
-            {id: 2, name: "Смартфоны"},
-            {id: 3, name: "Ноутбуки"},
-            {id: 4, name: "Телевизоры"},
+            // {id: 1, name: "Холодильники"},
+            // {id: 2, name: "Смартфоны"},
+            // {id: 3, name: "Ноутбуки"},
+            // {id: 4, name: "Телевизоры"},
         ]
         this._brands = [
-            {id: 1, name: "Samsung"},
-            {id: 2, name: "Apple"},
-            {id: 3, name: "Lenovo"},
-            {id: 4, name: "Asus"},
-
-
+            // {id: 1, name: "Samsung"},
+            // {id: 2, name: "Apple"},
+            // {id: 3, name: "Lenovo"},
+            // {id: 4, name: "Asus"},
         ]
         this._devices = [
-            {
-                id: 1,
-                name: "IPhone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
-            },
-            {
-                id: 2,
-                name: "IPhone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
-            },
-            {
-                id: 3,
-                name: "IPhone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
-            },
-            {
-                id: 4,
-                name: "IPhone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
-            },
-            {
-                id: 5,
-                name: "IPhone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
-            },
-            {
-                id: 6,
-                name: "IPhone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
-            },
-            {
-                id: 7,
-                name: "IPhone 12 pro",
-                price: 25000,
-                rating: 5,
-                img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
-            },
+            // {
+            //     id: 1,
+            //     name: "IPhone 12 pro",
+            //     price: 25000,
+            //     rating: 5,
+            //     img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
+            // },
+            // {
+            //     id: 2,
+            //     name: "IPhone 12 pro",
+            //     price: 25000,
+            //     rating: 5,
+            //     img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
+            // },
+            // {
+            //     id: 3,
+            //     name: "IPhone 12 pro",
+            //     price: 25000,
+            //     rating: 5,
+            //     img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
+            // },
+            // {
+            //     id: 4,
+            //     name: "IPhone 12 pro",
+            //     price: 25000,
+            //     rating: 5,
+            //     img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
+            // },
+            // {
+            //     id: 5,
+            //     name: "IPhone 12 pro",
+            //     price: 25000,
+            //     rating: 5,
+            //     img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
+            // },
+            // {
+            //     id: 6,
+            //     name: "IPhone 12 pro",
+            //     price: 25000,
+            //     rating: 5,
+            //     img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
+            // },
+            // {
+            //     id: 7,
+            //     name: "IPhone 12 pro",
+            //     price: 25000,
+            //     rating: 5,
+            //     img: 'https://cdnn21.img.ria.ru/images/07e5/0a/1e/1757022126_0:0:3072:2048_1440x900_80_1_1_9338b7928b524995666533c6f7a262c5.jpg.webp?source-sid=rian_photo'
+            // },
         ]
 
         this._selectedType ={}
